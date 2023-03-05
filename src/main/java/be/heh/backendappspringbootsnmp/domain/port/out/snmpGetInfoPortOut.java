140e0b3a4b9aebfd95d987efa11b2303b040578e
SimkeAnthony <anthony.simke@std.heh.be>
2023-03-05 09:16:15 +0100

    ajout de ladéfinition de la méthode getSomeInfo (no test)

--- src/main/java/be/heh/backendappspringbootsnmp/domain/port/out/snmpGetInfoPortOut.java
+++ src/main/java/be/heh/backendappspringbootsnmp/domain/port/out/snmpGetInfoPortOut.java
@@ -2,7 +2,10 @@ package be.heh.backendappspringbootsnmp.domain.port.out;
 
 import be.heh.backendappspringbootsnmp.domain.entities.MachineEntity;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 public interface snmpGetInfoPortOut {
 
-    public MachineEntity getSomeInfo(String ipAddress);
+    public MachineEntity getSomeInfo(String ipAddress) throws IOException;
 }
--- src/main/java/be/heh/backendappspringbootsnmp/infra/adaptateur/secondary/snmpGet.java
+++ src/main/java/be/heh/backendappspringbootsnmp/infra/adaptateur/secondary/snmpGet.java
@@ -3,8 +3,16 @@ package be.heh.backendappspringbootsnmp.infra.adaptateur.secondary;
 import be.heh.backendappspringbootsnmp.domain.entities.MachineEntity;
 import be.heh.backendappspringbootsnmp.domain.port.out.snmpGetInfoPortOut;
 import lombok.Getter;
+import org.snmp4j.CommunityTarget;
+import org.snmp4j.PDU;
+import org.snmp4j.Snmp;
+import org.snmp4j.TransportMapping;
+import org.snmp4j.event.ResponseEvent;
 import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.*;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,8 +29,61 @@ public class snmpGet implements snmpGetInfoPortOut {
     @Getter
     private static String community = "public";
     @Override
-    public MachineEntity getSomeInfo(String ipAddress) {
+    public MachineEntity getSomeInfo(String ipAddress) throws IOException {
 
+        System.out.println("Start snmp Get");
+
+        TransportMapping transport = new DefaultUdpTransportMapping();
+        transport.listen();
+
+        //create the target (host to reach)
+        CommunityTarget communityTarget = new CommunityTarget();
+        communityTarget.setCommunity(new OctetString(getCommunity()));
+        communityTarget.setVersion(getSnmpVersion());
+        communityTarget.setAddress(new UdpAddress(ipAddress+"/"+getPort()));
+        communityTarget.setRetries(2);
+        communityTarget.setTimeout(1000);
+
+        //create the PDU Tram
+        PDU pdu = new PDU();
+        for (String oid : getOidList()){
+            pdu.add(new VariableBinding(new OID(oid)));
+        }
+
+        pdu.setType(PDU.GET);
+        pdu.setRequestID(new Integer32(1));
+
+        //create object snmp for sending request to agent
+        Snmp snmp = new Snmp(transport);
+
+        System.out.println("Sending pdu to agent ...");
+        ResponseEvent responseEvent = snmp.get(pdu,communityTarget);
+
+        //manage process agent
+        if(responseEvent!=null){
+            System.out.println("Get response from agent");
+            PDU responseAgent = responseEvent.getResponse();
+
+            if(responseAgent!=null){
+                int errorStatus = responseAgent.getErrorStatus();
+                int errorIndex = responseAgent.getErrorIndex();
+                String errorStatusText = responseAgent.getErrorStatusText();
+
+                if(errorStatus==PDU.noError){
+                    System.out.println("Response GET : "+responseAgent.getVariableBindings());
+                }else{
+                    System.out.println("Error : request failed!");
+                    System.out.println("Error status : "+errorStatus);
+                    System.out.println("Error index : "+errorIndex);
+                    System.out.println("Error status text : "+errorStatusText);
+                }
+            }else{
+                System.out.println("Error response from agent is null!");
+            }
+        }else{
+            System.out.println("Error agent timeout ...");
+        }
+        snmp.close();
         return null;
     }
 }
