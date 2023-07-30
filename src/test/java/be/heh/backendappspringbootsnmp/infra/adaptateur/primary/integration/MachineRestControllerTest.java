19a4d0ff2857511dd31defe9b1610ab2d9f4fd5d
SimkeAnthony <anthony.simke@std.heh.be>
2023-07-30 19:37:02 +0200

    ajout du test de la classe OIDPersistenceAdaptater

--- src/test/java/be/heh/backendappspringbootsnmp/infra/adaptateur/primary/integration/MachineRestControllerTest.java
+++ src/test/java/be/heh/backendappspringbootsnmp/infra/adaptateur/primary/integration/MachineRestControllerTest.java
@@ -3,8 +3,11 @@ package be.heh.backendappspringbootsnmp.infra.adaptateur.primary.integration;
 import be.heh.backendappspringbootsnmp.domain.entities.MachineEntity;
 import be.heh.backendappspringbootsnmp.domain.port.in.MachinePortIn;
 import be.heh.backendappspringbootsnmp.infra.adaptateur.primary.MachineRestController;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.nmap4j.core.nmap.NMapExecutionException;
+import org.nmap4j.core.nmap.NMapInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,9 +37,10 @@ public class MachineRestControllerTest {
     private List<MachineEntity> machineEntities=new ArrayList<>();
 
     @Test
-    public void testGetAllMachineEntities() throws Exception {
-
+    @Order(1)
+    public void getAllMachineEntitiesTest() throws Exception {
         //Records
+        machineEntities.clear();
         machineEntities.add(new MachineEntity("machine1.local","windows",true));
         machineEntities.add(new MachineEntity("machine2.local","linux ubuntu",false));
         machineEntities.add(new MachineEntity("machine3.local","oracle",false));
@@ -52,6 +57,29 @@ public class MachineRestControllerTest {
                 .andExpect(jsonPath("$[1].os",is("linux ubuntu")))
                 .andExpect(jsonPath("$[0].snmp",is(true)))
                 .andReturn();
+    }
+    @Test
+    @Order(2)
+    public void rescanTest() throws Exception {
+        //Records
+        machineEntities.clear();
+        machineEntities.add(new MachineEntity("machine6.local","windows server",true));
+        machineEntities.add(new MachineEntity("machine5.local","linux RHEL 7",false));
+        machineEntities.add(new MachineEntity("machine4.local","oracle",false));
+
+        //Stub
+        Mockito.when(machinePortIn.rescanNetwork()).thenReturn(machineEntities);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/scan")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[2].hostname",is("machine4.local")))
+                .andExpect(jsonPath("$[1].os",is("linux RHEL 7")))
+                .andExpect(jsonPath("$[0].snmp",is(true)))
+                .andReturn();
+
     }
 
 }
---
+++ src/test/java/be/heh/backendappspringbootsnmp/infra/adaptateur/secondary/unitaire/OIDPersistenceAdaptaterTest.java
@@ -0,0 +1,124 @@
+package be.heh.backendappspringbootsnmp.infra.adaptateur.secondary.unitaire;
+
+import be.heh.backendappspringbootsnmp.domain.entities.MOManager;
+import be.heh.backendappspringbootsnmp.domain.entities.MOVariable;
+import be.heh.backendappspringbootsnmp.infra.adaptateur.secondary.OIDPersistenceAdaptater;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.snmp4j.agent.mo.MOAccessImpl;
+import org.snmp4j.smi.OID;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+
+@WebMvcTest(OIDPersistenceAdaptater.class)
+public class OIDPersistenceAdaptaterTest {
+    @MockBean
+    private OIDPersistenceAdaptater oidPersistenceAdaptater;
+
+    @Test
+    public void getMOManagerByOIDTest(){
+        OID oid = new OID("1.2.3.4.5.6.7.8.9");
+        MOManager moManager = new MOManager("default","1.2.3");
+
+        Mockito.when(oidPersistenceAdaptater.getMOManagerByOID(oid)).thenReturn(moManager);
+
+        MOManager actualMOManager = oidPersistenceAdaptater.getMOManagerByOID(oid);
+
+        Assertions.assertEquals("1.2.3",actualMOManager.getOidRoot());
+        Assertions.assertEquals("default",actualMOManager.getName());
+    }
+
+    @Test
+    public void getOIDHostnameTest(){
+        String oidHostname = "1.3.4.12.6.13.2.4";
+
+        Mockito.when(oidPersistenceAdaptater.getOIDHostname()).thenReturn(oidHostname);
+
+        String actualOIDHostname = oidPersistenceAdaptater.getOIDHostname();
+
+        Assertions.assertEquals("1.3.4.12.6.13.2.4",actualOIDHostname);
+    }
+
+    @Test
+    public void getOidNumberIndexOfTableTest(){
+        String tableName = "defaultTable";
+        Pair<String,String> numberIndex = new Pair<>("1.2.3.4","1.2.3.4.1");
+
+        Mockito.when(oidPersistenceAdaptater.getOidNumberIndexOfTable(tableName)).thenReturn(numberIndex);
+
+        Pair<String,String> actualNumberIndex = oidPersistenceAdaptater.getOidNumberIndexOfTable(tableName);
+
+        Assertions.assertEquals("1.2.3.4",actualNumberIndex.getValue0());
+        Assertions.assertEquals("1.2.3.4.1",actualNumberIndex.getValue1());
+    }
+
+    @Test
+    public void getColumnOfTableTest(){
+        String tableName = "defaultTable";
+
+        List<MOVariable> columns = Arrays.asList(
+                new MOVariable("name1","1.2.3.4.1.1", MOAccessImpl.ACCESS_READ_ONLY,"desc1"),
+                new MOVariable("name2","1.2.3.4.1.2", MOAccessImpl.ACCESS_READ_ONLY,"desc2"),
+                new MOVariable("name3","1.2.3.4.1.3", MOAccessImpl.ACCESS_READ_ONLY,"desc3")
+        );
+
+        Mockito.when(oidPersistenceAdaptater.getColumnOfTable(tableName)).thenReturn(columns);
+
+        List<MOVariable> actualColumns = oidPersistenceAdaptater.getColumnOfTable(tableName);
+
+        Assertions.assertEquals(MOAccessImpl.ACCESS_READ_ONLY,actualColumns.get(2).getMoAccess());
+        Assertions.assertEquals("desc1",actualColumns.get(0).getDescription());
+        Assertions.assertEquals(3,actualColumns.size());
+        Assertions.assertEquals("name2",actualColumns.get(1).getName());
+        Assertions.assertEquals("1.2.3.4.1.2",actualColumns.get(1).getOid());
+    }
+
+    @Test
+    public void getOIDByNameTest(){
+        String name = "defaultName";
+        MOManager moManager = new MOManager("defaultName","1.2.3.4");
+
+        String oid = "1.2.3.4.1.2";
+
+        Mockito.when(oidPersistenceAdaptater.getOIDByName(name,moManager)).thenReturn(oid);
+
+        String actualOID = oidPersistenceAdaptater.getOIDByName(name,moManager);
+
+        Assertions.assertEquals("1.2.3.4.1.2",actualOID);
+    }
+
+    @Test
+    public void getOIDColumnByNameTest(){
+        String columnName = "defaultName";
+        MOManager moManager = new MOManager("defaultName","1.2.3.4");
+
+        String oid = "1.2.3.4.1.2";
+
+        Mockito.when(oidPersistenceAdaptater.getOIDColumnByName(columnName,moManager)).thenReturn(oid);
+
+        String actualOID = oidPersistenceAdaptater.getOIDColumnByName(columnName,moManager);
+
+        Assertions.assertEquals("1.2.3.4.1.2",actualOID);
+    }
+
+    @Test
+    public void getMOManagerByNameTest(){
+        String name = "defaultName";
+
+        MOManager moManager = new MOManager("defaultName","1.2.3.4");
+
+        Mockito.when(oidPersistenceAdaptater.getMOManagerByName(name)).thenReturn(moManager);
+
+        MOManager actualMOManager = oidPersistenceAdaptater.getMOManagerByName(name);
+
+        Assertions.assertEquals("defaultName",actualMOManager.getName());
+        Assertions.assertEquals("1.2.3.4",actualMOManager.getOidRoot());
+    }
+
+}
