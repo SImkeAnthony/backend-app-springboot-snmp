f61e81961580d7b28a48b0811180c74b6be7dff0
SimkeAnthony <anthony.simke@std.heh.be>
2023-09-01 10:58:33 +0200

    modification de la configuration des cors (basée sur spring security)

--- src/main/java/be/heh/backendappspringbootsnmp/SecurityWebConfiguration.java
+++ src/main/java/be/heh/backendappspringbootsnmp/SecurityWebConfiguration.java
@@ -3,32 +3,38 @@ package be.heh.backendappspringbootsnmp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNullApi;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
 @Configuration
-public class SecurityWebConfiguration {
+@EnableWebSecurity
+@EnableWebMvc
+public class SecurityWebConfiguration{
 
     @Autowired
     private Environment env;
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                String origin = env.getProperty("react.origin");
-                registry.addMapping("/**")
-                        .allowedOrigins("http://172.168.8.252:3000")
-                        .allowedOrigins("http://snmp-nms:3000")
-                        .allowedOrigins("http://"+origin)
-                        .allowedOrigins("https://"+origin)
-                        .allowedHeaders("Access-Control-Allow-Origin")
-                        .exposedHeaders("Access-Control-Allow-origin")
-                        .allowedMethods("GET","POST")
-                        .allowCredentials(true);
-            }
-        };
+    CorsConfigurationSource corsConfigurationSource() {
+        String origin = env.getProperty("react.origin");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://"+origin,"http://172.16.8.252:3000","http://snmp-nms:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+        http.cors().configurationSource(corsConfigurationSource());
+        return http.build();
     }
 }
