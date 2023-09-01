561bf4dfd5d06178107c459425fed2f90e2dabb5
SimkeAnthony <anthony.simke@std.heh.be>
2023-09-01 10:25:22 +0200

    akout en dure les origins pour la production

--- src/main/java/be/heh/backendappspringbootsnmp/SecurityWebConfiguration.java
+++ src/main/java/be/heh/backendappspringbootsnmp/SecurityWebConfiguration.java
@@ -3,6 +3,7 @@ package be.heh.backendappspringbootsnmp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.core.env.Environment;
@@ -19,9 +20,12 @@ public class SecurityWebConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 String origin = env.getProperty("react.origin");
                 registry.addMapping("/**")
+                        .allowedOrigins("http://172.168.8.252:3000")
+                        .allowedOrigins("http://snmp-nms:3000")
                         .allowedOrigins("http://"+origin)
                         .allowedOrigins("https://"+origin)
                         .allowedHeaders("Access-Control-Allow-Origin")
+                        .exposedHeaders("Access-Control-Allow-origin")
                         .allowedMethods("GET","POST")
                         .allowCredentials(true);
             }
